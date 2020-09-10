3c9389c796f90c3470af4c64d7d7094c3e2d8333
Sebastian Wiendlocha <sebastian.wiendlocha@cloudpilots.com>
2020-09-10 22:35:20 +0200

    added functions needed by wrapper and model generator

---
+++ utilities.js
@@ -0,0 +1,31 @@
+/**
+ * converts a key from snake_case, kebap-case, ALL_CAPS or a combination thereof to camelCase
+ */
+function toCamelCase(key) {
+  // I don't like keys SCREAMING at me, so lowercase if the whole key is uppercase
+  const keyWithoutLowerCasedChars = key
+    .split("")
+    .filter((c) => c === c.toUpperCase())
+    .join("");
+  if (keyWithoutLowerCasedChars === key) {
+    key = key.toLowerCase();
+  }
+
+  // replace snake_case with camelCase
+  let camel = key.replace(/_(.)/g, (m, c) => c.toUpperCase());
+
+  // replace kebap-case with camelCase
+  camel = camel.replace(/-(.)/g, (m, c) => c.toUpperCase());
+
+  // lowercase first char
+  // this throws index error if key is an empty string, which is ok for this our use case for now
+  camel = camel[0].toLowerCase() + camel.slice(1);
+
+  return camel;
+}
+
+function isInt(n) {
+  return n % 1 === 0;
+}
+
+module.exports = { isInt, toCamelCase };
